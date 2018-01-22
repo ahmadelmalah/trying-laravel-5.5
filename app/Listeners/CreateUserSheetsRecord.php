78e4f2b5bba5fa6d3cb53a469a963b7b66fa5710
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2018-01-22 22:28:04 +0200

    an Event for stack subscription

---
+++ app/Listeners/CreateUserSheetsRecord.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UsersubscripedToStack;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class CreateUserSheetsRecord
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UsersubscripedToStack  $event
+     * @return void
+     */
+    public function handle(UsersubscripedToStack $event)
+    {
+        //
+    }
+}
