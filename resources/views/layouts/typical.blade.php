bc29d8ebba6fee228e903189542c51460ce63b7a
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2018-02-24 19:16:00 +0200

    blade refactor

---
+++ resources/views/layouts/typical.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    @yield('content_title')
+                </div>
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+
+                    @yield('content_content')
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/my-stacks.blade.php
+++ resources/views/my-stacks.blade.php
@@ -1,37 +1,25 @@
-@extends('layouts.app')
+@extends('layouts.typical')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">My Stacks</div>
+@section('content_title', 'My Stacks')
 
-                <a href="{{ route('stack-create') }}" class="btn btn-primary">Create a new stack</a>
+@section('content_content')
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <a href="{{ route('stack-create') }}" class="btn btn-primary btn-block">Create a new stack</a>
+    </div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @foreach ($stacks as $stack)
-                        <div class="panel panel-info">
-                            <div class="panel-heading">
-                                <h3 class="panel-title">{{ $stack->name }}</h3>
-                            </div>
-                            <div class="panel-body">
-                            {{ $stack->description }}
-                            </div>
-                            <div class="panel-footer">
-                                <a href="{{ route('stack-status', ['stack' => $stack->id]) }}" class="btn btn-default">View Stack Status</a>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
+    @foreach ($stacks as $stack)
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">{{ $stack->name }}</h3>
+            </div>
+            <div class="panel-body">
+            {{ $stack->description }}
+            </div>
+            <div class="panel-footer">
+                <a href="{{ route('stack-status', ['stack' => $stack->id]) }}" class="btn btn-default">View Stack Status</a>
             </div>
         </div>
-    </div>
+    @endforeach
 </div>
 @endsection
