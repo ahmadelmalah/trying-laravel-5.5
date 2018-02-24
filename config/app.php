98b2a89a53fb140953ac058be2b93c1c08772fc9
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2018-02-24 19:38:13 +0200

    changing app name to Lessontrainer

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Learnsheets'),
+    'name' => env('APP_NAME', 'Lessontrainer'),
 
     /*
     |--------------------------------------------------------------------------
