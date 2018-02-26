4892f26c0716a63384f432d1e366eeacf7c2661b
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2018-02-26 22:34:02 +0200

    StackComponent

--- resources/views/my-stacks.blade.php
+++ resources/views/my-stacks.blade.php
@@ -9,17 +9,7 @@
     </div>
 
     @foreach ($stacks as $stack)
-        <div class="panel panel-info">
-            <div class="panel-heading">
-                <h3 class="panel-title">{{ $stack->name }}</h3>
-            </div>
-            <div class="panel-body">
-            {{ $stack->description }}
-            </div>
-            <div class="panel-footer">
-                <a href="{{ route('stack-status', ['stack' => $stack->id]) }}" class="btn btn-default">View Stack Status</a>
-            </div>
-        </div>
+        @include('StackComponent')
     @endforeach
 </div>
 @endsection
