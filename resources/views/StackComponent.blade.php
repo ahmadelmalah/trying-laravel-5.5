50365f29f1a69b15fa5edd3b0d043475e052e515
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2018-02-26 22:48:44 +0200

    Adding button for making stack public

--- resources/views/StackComponent.blade.php
+++ resources/views/StackComponent.blade.php
@@ -7,5 +7,8 @@
     </div>
     <div class="panel-footer">
         <a href="{{ route('stack-status', ['stack' => $stack->id]) }}" class="btn btn-default">View Stack Status</a>
+        @if($stack->type != 3 && $stack->created_by == Auth()->User()->id)
+            <a href="{{ route('stack-status', ['stack' => $stack->id]) }}" class="btn btn-default">Make Public</a>
+        @endif
     </div>
 </div>
