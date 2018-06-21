97a28b9b8e69cb2cd6e74541c6664b857ffbbd09
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2018-06-21 20:24:48 +0200

    Solving a minor issue

--- resources/views/practice/type_multipleoptions.blade.php
+++ resources/views/practice/type_multipleoptions.blade.php
@@ -1,8 +1,8 @@
 @if($reveal == true)
 <div class="input-group">
     @foreach ($answer as $option=>$value)
-        <input type="checkbox" name="option[]" @if($value == "selected")checked @endif disabled>
-        <label for="option[]">{{ $option }}</label>
+        <input type="checkbox" name="userResponse[]" @if($value == "selected")checked @endif disabled>
+        <label for="userResponse[]">{{ $option }}</label>
         <br>
     @endforeach
     <a href="{{ route('practice', ['stack' => $stack->id]) }}" class="btn btn-primary">Ahh OK!</a>
@@ -10,8 +10,8 @@
 @else
 <div class="input-group">
     @foreach ($answer as $option=>$value)
-        <input type="checkbox" name="option[]">
-        <label for="option[]">{{ $option }}</label>
+        <input type="checkbox" name="UserResponse[]" value="{{$loop->index}}">
+        <label for="UserResponse">{{ $option }}</label>
         <br>
     @endforeach
     <button class="btn btn-primary" type="submit">Submit & Next</button>
