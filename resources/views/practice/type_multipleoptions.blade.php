3b65371489b38f7ea1ebd6a2dbee2bd84c839c77
Ahmed Elmalah <ahmedelmalah@hotmail.com>
2018-06-21 18:58:01 +0200

    Special form for multiple option quetions

--- resources/views/practice/type_multipleoptions.blade.php
+++ resources/views/practice/type_multipleoptions.blade.php
@@ -1,17 +1,19 @@
 @if($reveal == true)
 <div class="input-group">
-    <input type="text" name="UserResponse" class="form-control" 
-    placeholder="Your Answer .." value="{{ $answer }}" disabled>
-    <span class="input-group-btn">
-        <a href="{{ route('practice', ['stack' => $stack->id]) }}" class="btn btn-primary">Ahh OK!</a>
-    </span>
+    @foreach ($answer as $option=>$value)
+        <input type="checkbox" name="option[]" @if($value == "selected")checked @endif disabled>
+        <label for="option[]">{{ $option }}</label>
+        <br>
+    @endforeach
+    <a href="{{ route('practice', ['stack' => $stack->id]) }}" class="btn btn-primary">Ahh OK!</a>
 </div>
 @else
 <div class="input-group">
-    <input type="text" name="UserResponse" class="form-control" 
-    placeholder="Your Answer .." value="{{ $answer }}" autofocus="autofocus" autocomplete="off" required>
-    <span class="input-group-btn">
-        <button class="btn btn-primary" type="submit">Submit & Next</button>
-    </span>
+    @foreach ($answer as $option=>$value)
+        <input type="checkbox" name="option[]">
+        <label for="option[]">{{ $option }}</label>
+        <br>
+    @endforeach
+    <button class="btn btn-primary" type="submit">Submit & Next</button>
 </div>
 @endif
